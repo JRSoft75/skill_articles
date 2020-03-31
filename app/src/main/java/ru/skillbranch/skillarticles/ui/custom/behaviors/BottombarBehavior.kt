aad980186632c9ee2a3b8ffd3abd69708636d3f6
Ryabchenko E <johny@mail.ru>
2020-03-31 10:24:33 +0300

    revert isOpen

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -34,7 +34,8 @@ class BottombarBehavior: CoordinatorLayout.Behavior<Bottombar>(){
         // dy<0 scroll down
         // dy>0 scroll up
         val offset =  MathUtils.clamp(child.translationY + dy,0f,child.height.toFloat())
-        if(child.isClose && offset != child.translationY) child.translationY = offset
+    //    if(child.isClose && offset != child.translationY) child.translationY = offset
+        if(offset != child.translationY) child.translationY = offset
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
     }
 
