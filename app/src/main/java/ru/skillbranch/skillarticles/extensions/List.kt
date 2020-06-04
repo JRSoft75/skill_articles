9b660feb0d956c8143f1ac9308e80e255142f9f1
Ryabchenko E <johny@mail.ru>
2020-06-04 10:46:14 +0300

    fix list

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/List.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/List.kt
@@ -6,18 +6,37 @@ fun List<Pair<Int, Int>>.groupByBounds(list: List<Pair<Int, Int>> ): List<Mutabl
 
     var lastIndex = 0
     list.forEach{
-        val (boundStart, boundEnd) = it
-        val (a, b ) = this[lastIndex]
-        while(boundStart < a || boundEnd > b){ //если начало Bound меньше начала диапазона или конец Bound больше конца диапазона, значит диапазон входит в Bounds
-            lastIndex++ //переход на следующую пару
-            currentList = mutableListOf()
-            currentList.add(it)
-            if(boundEnd in a..b){ //последний элемент группы
-                result.add(currentList)
-                break
-            }
-        }
+        var (boundStart, boundEnd) = it  //60-100
+        currentList = mutableListOf()
 
+        var (listStart, listEnd ) = this[lastIndex] // 70-100
+        while(listStart in boundStart..boundEnd || listEnd in boundStart..boundEnd){ //если начало диапазона или конец входят в Bounds
+            if(boundEnd in listStart..listEnd){ //последний элемент группы
+                currentList.add(listStart to boundEnd) //
+                result.add(currentList)
+                if(boundEnd == listEnd){
+                    lastIndex++ //переход на следующую пару 3
+                }
+                //иначе остаемся на этой паре и берем следующий диапазон
+                break
+            }else if(boundStart in listStart..listEnd){
+                currentList.add(boundStart to listEnd) //
+                lastIndex++ //переход на следующую пару 2
+            }else{
+                currentList.add(it) //добавляем пару как есть
+                lastIndex++ //переход на следующую пару 2
+            }
+
+            listStart= this[lastIndex].first //22
+            listEnd= this[lastIndex].second //30
+        }
+/**
+ * (2,5)  (8,10)
+ * (10,20) 22-30
+ * 45-50
+ *
+ *
+* */
     }
     return result
 }
