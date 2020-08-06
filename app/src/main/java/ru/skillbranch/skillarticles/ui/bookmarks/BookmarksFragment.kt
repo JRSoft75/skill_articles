90e893130eae35b5c6c9a7811636c325b27db945
Ryabchenko E <johny@mail.ru>
2020-08-06 21:28:27 +0300

    v6 work

--- app/src/main/java/ru/skillbranch/skillarticles/ui/bookmarks/BookmarksFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/bookmarks/BookmarksFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.viewmodels.bookmarks.BookmarksViewModel
 
@@ -26,7 +26,7 @@ class BookmarksFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(BookmarksViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(BookmarksViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
